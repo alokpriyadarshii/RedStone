d08a07f43f9f3dbd0fe5acee4882fca1d5bf11fd
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-01-18 10:16:54 +0530

    Create chronicle_util_test.rb

---
+++ test/test/chronicle_util_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+class ChronicleUtilTest < Minitest::Test
+  def test_parse_kv_pairs_strips_whitespace
+    meta = Chronicle::Util.parse_kv_pairs([" key = value "])
+    assert_equal({"key" => "value"}, meta)
+  end
+end
